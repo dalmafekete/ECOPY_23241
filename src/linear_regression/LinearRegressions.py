c2696bff753c050d29e1a3db213bb3a9cb64732c
dalmafekete <dalmus111@gmail.com>
2023-11-18 18:06:28 +0100

    Yixorw

--- src/linear_regression/LinearRegressions.py
+++ src/linear_regression/LinearRegressions.py
@@ -221,13 +221,51 @@ class LinearRegressionGLS:
         X = self.right_hand_side
         y = self.left_hand_side
         X = np.column_stack((np.ones(X.shape[0]), X))
-        XTX = np.dot(X.T, X)
-        y_pred = X @ self.beta_gls
+
+        XTVX = X.T @ self.V_inv @ X
+
+        y_pred = np.dot(X, self.beta_gls)
         residuals = y - y_pred
-        se = np.sqrt(residuals ** 2 / self.V_inv)
-        t_stats = self.beta_gls / se
-        p_values = 2 * (1 - t.cdf(np.abs(t_stats)))
+        mse = residuals.T @ residuals/ (X.shape[0] - X.shape[1])
+
+        t_stats = self.beta_gls / np.sqrt(np.diag(mse * np.linalg.inv(XTVX)))
+        p_values = 2 * (1 - t.cdf(np.abs(t_stats), (X.shape[0] - X.shape[1])))
 
         p_values_series = pd.Series(p_values[0:], name="P-values for the corresponding coefficients")
 
         return p_values_series
+
+    def get_wald_test_result(self, restriction_matrix):
+        X = self.right_hand_side
+        y = self.left_hand_side
+        X = np.concatenate((np.ones((X.shape[0], 1)), self.right_hand_side), axis=1)
+        R = np.asmatrix(restriction_matrix)
+
+        n = X.shape[0]
+        df2 = n - X.shape[1]
+        residuals = y - np.dot(X, self.beta_gls)
+        sse = np.dot(residuals.T, residuals)/df2
+
+        RBr = R @ self.beta_gls
+        df1 = R.shape[0]
+        wald_one = R @ np.linalg.inv(X.T @ self.V_inv @ X) @ R.T
+        wald_test = RBr @ np.linalg.inv(wald_one) @ RBr.T/ df1/ sse
+
+        p_value = 1 - f.cdf(wald_test.item(), df1, df2)
+        result_string = f"Wald: {wald_test.item():.3f}, p-value: {p_value:.3f}"
+        return result_string
+
+    def get_model_goodness_values(self):
+        X = self.right_hand_side
+        y = self.left_hand_side
+        n = X.shape[0]
+        X = np.column_stack((np.ones(n), X))
+
+        sse = y.T @ self.V_inv @ X @ self.beta_gls
+        sst = y.T @ self.V_inv @ y
+
+        crs = 1 - sse / sst
+        ars = 1 - (1 - crs) * (n - 1) / (n - X.shape[1])
+
+        result_string = f"Centered R-squared: {crs:.3f}, Adjusted R-squared: {ars:.3f}"
+        return result_string
